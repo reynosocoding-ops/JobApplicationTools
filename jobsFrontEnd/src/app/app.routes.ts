1c1da62de1272ed84d2ba20b8af0747269fe7099
Jesus <reynosoprof@gmail.com>
2026-04-05 13:11:06 -0700

    created home paage component

--- jobsFrontEnd/src/app/app.routes.ts
+++ jobsFrontEnd/src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
 import { ResumeUploadPageComponent } from './resume-upload-page/resume-upload-page.component';
+import {AnalyzeListingPageComponent} from "./analyze-listing-page/analyze-listing-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 export const routes: Routes = [
-    {path: 'uploadresume', component: ResumeUploadPageComponent}
+    {path: 'uploadresume', component: ResumeUploadPageComponent},
+    {path: 'analyzelisting', component: AnalyzeListingPageComponent},
+    {path: 'home', component: HomePageComponent},
+    {path: '', component: HomePageComponent},
 ];
+ 
